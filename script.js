69700fdda9e841698a68ea27913591dfdf52cf09
ABHINAV V A <84282995+JUSTMEHER@users.noreply.github.com>
2026-02-09 20:40:57 +0530

    Adjust loader timeout and play sound on load

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 window.addEventListener("load", () => {
+
+  const sound = document.getElementById("jetSound");
+
+  if (sound) {
+    sound.volume = 0.45;
+    sound.play().catch(()=>{});
+  }
+
+  setTimeout(() => {
+    document.getElementById("loader").style.opacity = 0;
+  }, 2700);
+
   setTimeout(() => {
     document.getElementById("loader").style.display = "none";
-  }, 2800);
+  }, 3300);
+
 });
